aa77adc87cec4803c933885ecaa60ff2f6ca8f5e
shireenarora <112525481+shireenarora@users.noreply.github.com>
2022-11-15 18:07:30 -0700

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class Ground{
+    constructor(x,y,w,h){
+      let options = {
+        isStatic: true,
+      }
+      this.body = Bodies.rectangle(x,y,w,h,options)
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      World.add(world,this.body)
+    }
+    show(){
+      var pos = this.body.position
+      push()
+      translate(pos.x,pos.y)
+      rectMode(CENTER)
+      stroke(255)
+      fill(127)
+      rect(0,0,this.w,this.h)
+      pop()
+    }
+  }
+  
